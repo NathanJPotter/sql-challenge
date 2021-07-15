c381dbce33bd0889be733014b57f5d23f0d7fea1
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-07-15 11:10:32 +1000

    Fixed some typos

--- EmployeesSQL/SQL tables.sql
+++ EmployeesSQL/SQL tables.sql
@@ -1,5 +1,7 @@
 CREATE TABLE departments (
 dept_no VARCHAR(30),
-dept_name VARCH(50) NOT NULL,
-PRIMART KEY (dept_no)
+dept_name VARCHAR(50) NOT NULL,
+PRIMARY KEY (dept_no)
 );
+
+SELECT * FROM departments
