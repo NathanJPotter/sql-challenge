bfa6e59252b7af4eb2521111323f03591f064706
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-07-15 13:23:51 +1000

    Lists department of each employ (plus info)

--- EmployeesSQL/SQL queries.sql
+++ EmployeesSQL/SQL queries.sql
@@ -34,3 +34,16 @@ LEFT JOIN employees
 ON dept_managers.emp_no = employees.emp_no
 ORDER BY emp_no
 
+--4. List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+
+SELECT
+employees.emp_no,
+employees.last_name,
+employees.first_name,
+dept_emp.dept_no,
+departments.dept_name
+FROM employees 
+INNER JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no=dept_emp.dept_no
+order by emp_no;
