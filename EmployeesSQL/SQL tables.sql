9abc7ac83e3d8a1ff9696a7ffd8302a82e1f378d
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-07-15 13:02:28 +1000

    Tired specifying foreign keys but didn't work

--- EmployeesSQL/SQL tables.sql
+++ EmployeesSQL/SQL tables.sql
@@ -1,6 +1,6 @@
 -- Create departments table
 
-DROP TABLE deptartments
+DROP TABLE departments
 
 CREATE TABLE departments (
 dept_no VARCHAR(30) NOT NULL,
@@ -78,3 +78,26 @@ CREATE TABLE titles(
 );
 
 SELECT * FROM titles
+
+-- Adding foreign keys
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES dept_manager (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES salaries (emp_no);
+
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_no FOREIGN KEY(emp_no)
+REFERENCES dept_manager (emp_no);
+
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES titles (title_id);
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
