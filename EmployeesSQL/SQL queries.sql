2ce4658ae1539de52dc0abbb062fc33199991039
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-07-15 13:29:56 +1000

    Queried employees in Sales department

--- EmployeesSQL/SQL queries.sql
+++ EmployeesSQL/SQL queries.sql
@@ -55,3 +55,18 @@ employees.last_name,
 employees.sex 
 FROM employees
 WHERE first_name = 'Hercules' AND last_name like 'B%';
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and 
+-- department name.
+
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name,
+dept_emp.dept_no
+FROM employees 
+LEFT JOIN dept_emp 
+ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments 
+ON departments.dept_no=dept_emp.dept_no
+WHERE departments.dept_name='Sales';
