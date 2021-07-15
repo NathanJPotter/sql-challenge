160d453df21aa6ba4f206022275a12450f2118ea
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-07-15 13:34:06 +1000

    Queried the frequency count employees who share the same last name

--- EmployeesSQL/SQL queries.sql
+++ EmployeesSQL/SQL queries.sql
@@ -86,3 +86,10 @@ INNER JOIN departments
 ON departments.dept_no=dept_emp.dept_no
 WHERE departments.dept_name in ('Sales', 'Development');
 
+-- 8. In descending order, list the frequency count of employee last names, 
+--i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC;
