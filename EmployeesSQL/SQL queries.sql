5a8d36b44fb86afaa91d0aaf80eeae58824e5d36
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-07-15 13:15:20 +1000

    Created list of employee details hired in 1986

--- EmployeesSQL/SQL queries.sql
+++ EmployeesSQL/SQL queries.sql
@@ -9,3 +9,12 @@ FROM employees
 LEFT JOIN salaries
 ON employees.emp_no = salaries.emp_no
 ORDER BY emp_no
+
+--2. List employees who were hired in 1986.
+
+SELECT first_name,
+last_name,
+hire_date 
+FROM employees
+WHERE DATE_PART('year',hire_date) = 1986
+ORDER BY emp_no;
