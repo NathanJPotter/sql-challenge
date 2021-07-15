36d885bf6c306e54cbc1de3c75dd1294069724b2
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-07-15 11:24:56 +1000

    Added drop table for dept_emp
    The import didn't work for some reason

--- EmployeesSQL/SQL tables.sql
+++ EmployeesSQL/SQL tables.sql
@@ -1,3 +1,5 @@
+-- Create departments table
+
 CREATE TABLE departments (
 dept_no VARCHAR(30),
 dept_name VARCHAR(50) NOT NULL,
@@ -5,3 +7,15 @@ PRIMARY KEY (dept_no)
 );
 
 SELECT * FROM departments
+
+DROP TABLE dept_emp
+
+-- Create dept_emp table
+
+CREATE TABLE dept_emp (
+emp_no INT NOT NULL,
+dept_no VARCHAR(50) NOT NULL,
+PRIMARY KEY (emp_no)
+);
+
+SELECT * FROM dept_emp
