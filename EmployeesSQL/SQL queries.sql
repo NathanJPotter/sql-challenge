ff9e83b640b4c1ca4518aeb0ab61be82f5626d66
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-07-15 13:17:19 +1000

    Tidied up script

--- EmployeesSQL/SQL queries.sql
+++ EmployeesSQL/SQL queries.sql
@@ -12,9 +12,9 @@ ORDER BY emp_no
 
 --2. List employees who were hired in 1986.
 
-SELECT first_name,
-last_name,
-hire_date 
+SELECT employees.first_name,
+employees.last_name,
+employees.hire_date 
 FROM employees
 WHERE DATE_PART('year',hire_date) = 1986
 ORDER BY emp_no;
