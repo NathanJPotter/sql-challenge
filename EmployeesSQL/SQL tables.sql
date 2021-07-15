a7be4eb20a3fe0f1f7b379b8d2e3a5469e4c8134
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-07-15 11:35:29 +1000

    Created dept_managers table

--- EmployeesSQL/SQL tables.sql
+++ EmployeesSQL/SQL tables.sql
@@ -8,14 +8,28 @@ PRIMARY KEY (dept_no)
 
 SELECT * FROM departments
 
-DROP TABLE dept_emp
 
 -- Create dept_emp table
+
+DROP TABLE dept_emp
 
 CREATE TABLE dept_emp (
 emp_no INT NOT NULL,
 dept_no VARCHAR(50) NOT NULL,
 PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 SELECT * FROM dept_emp
+
+-- Create table dept_managers
+
+DROP TABLE dept_managers
+
+CREATE TABLE dept_managers (
+ dept_no VARCHAR (10),
+ emp_no INT,
+);
+
+SELECT * FROM dept_managers
